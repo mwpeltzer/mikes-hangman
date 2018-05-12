fffb55f68fb938c8bc15d1c6a9ce35e58c4d9e31
Michael Peltzer <mwpeltzer@gmail.com>
2018-05-11 19:05:49 -0700

    added array with answers and random selector for the computer guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,24 @@
 
-// answers array
-var answers = ["Linkin Park", "Muse", "Sufjan Stevens"]
+// answers array stores answers in an array
+var computerChoices = ["Linkin Park", "Muse", "Sufjan Stevens"];
 
-console.log(answers);
+console.log(computerChoices);
 
-console.log(answers.length);
+console.log(computerChoices.length);
+
+//  Creating variables to hold the values of variables involved in the game
+var wins = 0;
+var currentAnswer = [];
+var guessesLeft = "";
+var lettersGuessed = "";
+
+// need to create a function that select one of the computerChoices randomnly
+
+var computerRandom = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log('Computer: ', computerRandom);	
+
+
+
+
+
 
