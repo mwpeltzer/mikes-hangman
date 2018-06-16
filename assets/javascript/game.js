9f3e1beaa995457c3e5e3c019259b17e7de50f9f
Michael Peltzer <mwpeltzer@gmail.com>
2018-06-16 16:45:31 -0700

    new javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,10 @@
 // answers array stores answers in an array
 var computerChoices = ["Linkin Park", "Muse", "Sufjan Stevens"];
 
+computerChoices.split("");
+console.log(choicesArray);
+
+
 console.log(computerChoices);
 console.log(computerChoices.length);
 
@@ -27,7 +31,28 @@ document.onkeyup = function(event) {
 	console.log(userGuess);
 
 
+	// Displaying tallies in html
 
+			 var html = //"<h1>The Psychic Game</h1>" + 
+        "<p>Guess What Letter I'm Thinking Of?</p>" +
+        "<p>Total Wins: " + 
+        wins + 
+        "</p>" +
+        "<p>Losses: " + 
+        losses + 
+        "</p>" +
+        "<p>Guesses Left: " + 
+        guessesLeft + 
+        "</p>" +
+        "<p>Your Guesses So Far: " +
+        guessesSofar +
+        "</p>";
+		
+		console.log()
+
+
+		// selects id from html
+	document.querySelector('#game').innerHTML = html; 
 
 
 
